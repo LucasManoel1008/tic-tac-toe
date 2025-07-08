b2a1b9ecbb755d3e718466e8e949b57f9abef048
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2025-07-07 22:33:51 -0300

    Refatoração: Organização do código para mante-lo escalável

--- javascript/game.js
+++ javascript/game.js
@@ -1,84 +1,117 @@
-let gameBoard;
-let currentPlayer;
-let gameOver;
-
-window.onload = function() {
-    initializeGame();    
-}
-
-
-function startGame() {    
-    let cells = document.querySelectorAll('.cell');  
-    if(gameOver){
-        alert('Game is already over. Please refresh the page to start a new game.');
-        return;
-    }    
-    cells.forEach((cell, index) =>{
-        cell.textContent = '';
-        cell.addEventListener('click', () => {
-            playOnBoard(index, cells);
-        });
-    })
-
-}
-function playOnBoard(index, cells) {
-        if (gameOver) return;
-        let row = Math.floor(index / 3);
-        let col = index % 3;
-        
-        if (gameBoard[row][col] === '') {
-            gameBoard[row][col] = currentPlayer;
-            cells[index].textContent = currentPlayer;            
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        }
-        checkWinner();
-
+    const Game = {
+    gameBoard: [],
+    currentPlayer: 'X',
+    gameOver: false,
     }
-    function checkWinner(){
-        if(gameBoard.every(row => row.every(cell => cell !== ''))) {
-            alert('It\'s a draw!');
-            gameOver = true;
-            return;
+    window.onload = function() {
+        initializeGame(); 
+    }
+
+    function initializeGame() {        
+        createBoard(); 
+        addCellsListener();
+        runGame();
+    }
+
+    function runGame() { 
+
+        if (isGameOver()) return    ();        
+        
+    }
+
+    function playOnBoard(index, cells) {
+
+            if(isGameOver()) return endGame();        
+            let row = getRow(index);
+            let col = getCol(index)        
+            markBoard(row, col);
+            setCellValue(cells, index);
+            checkWinner();
+
         }
-        for (let i = 0; i < 3; i++) {
-            // checar Linhas
-            if(gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2] && gameBoard[i][0] != '') {
-                gameOver = true;                
-                alert(`Player ${gameBoard[i][0]} wins!`);
+        function checkWinner(){
+            if(Game.gameBoard.every(row => row.every(cell => cell !== ''))) {
+                alert('It\'s a draw!');
+                Game.gameOver = true;
                 return;
             }
-            // checar Colunas
-            if(gameBoard[0][i] == gameBoard[1][i] && gameBoard[0][i] == gameBoard[2][i] && gameBoard[0][i] != '') {
-                gameOver = true;                
-                alert(`Player ${gameBoard[i][0]} wins!`);
+            for (let i = 0; i < 3; i++) {
+                // checar Linhas
+                if(Game.gameBoard[i][0] == Game.gameBoard[i][1] && Game.gameBoard[i][0] == Game.gameBoard[i][2] && Game.gameBoard[i][0] != '') {
+                    Game.gameOver = true;                
+                    alert(`Player ${Game.gameBoard[i][0]} wins!`);
+                    return;
+                }
+                // checar Colunas
+                if(Game.gameBoard[0][i] == Game.gameBoard[1][i] && Game.gameBoard[0][i] == Game.gameBoard[2][i] && Game.gameBoard[0][i] != '') {
+                    Game.gameOver = true;                
+                    alert(`Player ${Game.gameBoard[i][0]} wins!`);
+                    return;
+                }                        
+            }
+            // checar Diagonais
+            if(Game.gameBoard[0][0] == Game.gameBoard[1][1] && Game.gameBoard[0][0] == Game.gameBoard[2][2] && Game.gameBoard[0][0] != ''){
+                Game.gameOver = true;            
+                alert(`Player ${Game.gameBoard[0][0]} wins!`);
                 return;
-            }                        
+            }
+            if(Game.gameBoard[0][2] == Game.gameBoard[1][1] && Game.gameBoard[0][2] == Game.gameBoard[2][0] && Game.gameBoard[0][2] != ''){
+                Game.gameOver = true;            
+                alert(`Player ${Game.gameBoard[0][2]} wins!`);
+                return;
+            }
         }
-        // checar Diagonais
-        if(gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2] && gameBoard[0][0] != ''){
-            gameOver = true;            
-            alert(`Player ${gameBoard[0][0]} wins!`);
+
+
+        function isGameOver() {
+            return Game.gameOver;
+        }
+
+        function createBoard(){
+            Game.gameBoard = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+            ];
+            document.querySelectorAll('.cell')
+            .forEach((cell) => {
+                cell.textContent = '';       
+                cell.classList.remove('X', 'O'); 
+            });
+
+        }
+        function addCellsListener() {
+            let cells = document.querySelectorAll('.cell');
+            cells.forEach((cell, index) => {
+                cell.addEventListener('click', () => {
+                    playOnBoard(index, cells);
+                });
+            });
+        }
+
+        function endGame(){
             return;
         }
-        if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0] && gameBoard[0][2] != ''){
-            gameOver = true;            
-            alert(`Player ${gameBoard[0][2]} wins!`);
-            return;
+        function getCol(index){
+            return index % 3;
         }
-    }
-
-function initializeGame() {
-    gameBoard = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-    currentPlayer = 'X';
-    gameOver = false;
-    let cells = document.querySelectorAll('.cell');
-    cells.forEach((cell) => {
-        cell.textContent = '';        
-    });
-
-    startGame();
-}
+        function getRow(index){
+            return Math.floor(index / 3);
+        }
+        function resetGame() {
+            createBoard();
+            Game.currentPlayer = 'X';
+            Game.gameOver = false;
+            runGame();
+        }
+        function markBoard(row, col) {
+            if(Game.gameBoard[row][col] === ''){
+                Game.gameBoard[row][col] = Game.currentPlayer;                       
+                return true;
+            }
+            return false;
+        }
+        function setCellValue(cells,index){
+            cells[index].textContent = Game.currentPlayer;
+            Game.currentPlayer = Game.currentPlayer === 'X' ? 'O' : 'X'; 
+        }
